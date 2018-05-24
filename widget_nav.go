506aa45e6527299fa814d14d8419e413cccab680
japanoise <japanoise@tfwno.gf>
2018-05-24 13:31:29 +0100

    Extract navigation widget labels to const

--- widget_nav.go
+++ widget_nav.go
@@ -2,13 +2,20 @@ package main
 
 type NavigationWidget int
 
+const (
+	navStrLong      string = "Navigate: ←h ↓j ↑k →l"
+	navStrShort            = "Navigate: ←h ↓j"
+	navStrShortMore        = "↑k →l"
+	navStrWords            = "←←←←b w→→→→"
+)
+
 func (widget NavigationWidget) sizeForLayout(layout Layout) Size {
 	if layout.pressure > 1 {
 		return Size{0, 0}
 	}
 	layouts := map[int]string{
-		0: "Navigate: ←h ↓j ↑k →l",
-		1: "Navigate: ←h ↓j",
+		0: navStrLong,
+		1: navStrShort,
 	}
 	runeCount := 0
 	for _, _ = range layouts[layout.pressure] {
@@ -22,13 +29,13 @@ func (widget NavigationWidget) drawAtPoint(tab *DataTab, layout Layout, point Po
 	bg := style.default_bg
 	x_pos := point.x
 	if layout.pressure == 0 {
-		x_pos += drawStringAtPoint("Navigate: ←h ↓j ↑k →l", x_pos, point.y, fg, bg)
+		x_pos += drawStringAtPoint(navStrLong, x_pos, point.y, fg, bg)
 		x_pos = point.x + 10
-		x_pos += drawStringAtPoint("←←←←b w→→→→", x_pos, point.y+1, fg, bg)
+		x_pos += drawStringAtPoint(navStrWords, x_pos, point.y+1, fg, bg)
 	} else if layout.pressure == 1 {
-		x_pos += drawStringAtPoint("Navigate: ←h ↓j", x_pos, point.y, fg, bg)
+		x_pos += drawStringAtPoint(navStrShort, x_pos, point.y, fg, bg)
 		x_pos = point.x + 10
-		x_pos += drawStringAtPoint("↑k →l", x_pos, point.y+1, fg, bg)
+		x_pos += drawStringAtPoint(navStrShortMore, x_pos, point.y+1, fg, bg)
 	}
 	return Size{x_pos - point.x, 2}
 }
